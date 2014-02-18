03580f48f770063e5dbe27c7ba04a42a23d8b6c9
walk1ndude <lunarstrainut@gmail.com>
2014-02-18 19:04:15 +0400

    corrected sinogram

--- src/dicomreader.cpp
+++ src/dicomreader.cpp
@@ -21,7 +21,7 @@ typedef uint32_t u_int32_t;
 #define WINDOW_CONTOUR_IMAGE "contour"
 #define WINDOW_RADON_2D "sinogram2d"
 
-#define RADON_DEGREE_RANGE 45
+#define RADON_DEGREE_RANGE 180
 
 typedef struct _LoaderData {
     std::vector<cv::/*ocl::ocl*/Mat*> * ctImages;
@@ -56,6 +56,14 @@ public:
     }
 
     virtual void operator ()(const cv::Range & r) const {
+        int width = _loaderData.width / 2;
+        int height = _loaderData.height / 2;
+
+        int pad = std::max(width, height);
+
+        int widthPad = std::ceil((pad - width) / 2.0);
+        int heightPad = std::ceil((pad - height) / 2.0);
+
         for (register int i = r.start; i < r.end; i ++) {
 
             cv::Mat * data = new cv::Mat(_loaderData.width, _loaderData.height, _loaderData.type);
@@ -105,8 +113,7 @@ public:
                 }
             }
 
-            int width = _loaderData.width / 2;
-            int height = _loaderData.height / 2;
+
             cv::resize(*data, *data, cv::Size(width, height));
 
             //cv::GaussianBlur(*data, *data, cv::Size(9, 9), 5);
@@ -115,8 +122,6 @@ public:
 
             cv::Mat data8(width, height, CV_8UC1);
             data->convertTo(data8, CV_8UC1, 1/256.0);
-
-            //cv::threshold(data8, data8, 100, 0xFFFF, CV_THRESH_BINARY);
 
             _loaderData.images->at(i) = data;
 
@@ -130,42 +135,34 @@ public:
             cv::GaussianBlur(data8, *contourImage, cv::Size(5, 5), 5);
 
             //---radon---//
-          /*  int diag = ceil(sqrt(width * width + height * height));
-            int widthPad = ceil(diag - width) + 2;
-            int heightPad = ceil(diag - height) + 2;
 
+            cv::Mat imgPad(cv::Mat::zeros(height + heightPad, width + widthPad, CV_16UC1));
+            cv::Mat imgPadRotated(cv::Mat::zeros(height + heightPad, width + widthPad, CV_16UC1));
 
-            // all around borders is black, so no need in eleborate size calculation as above
-            //int widthPad = 10;
-            //int heightPad = 10;
-
-            cv::Mat * imgPad = new cv::Mat(cv::Mat::zeros(height + heightPad, width + widthPad, CV_16UC1));
             cv::Mat * sinogram = new cv::Mat(cv::Mat::zeros(height + heightPad, 0, CV_32FC1));
 
-            data->copyTo((*imgPad)(cv::Rect(ceil(widthPad / 2.0), ceil(heightPad / 2.0), width, height)));
+            data->copyTo(imgPad(cv::Rect(ceil(widthPad / 2.0), ceil(heightPad / 2.0), width, height)));
             cv::Point2i center = cv::Point2i((width + widthPad) / 2, (height + heightPad) / 2);
 
             cv::Mat rotationMatrix;
-            cv::Mat colSum(cv::Mat::zeros(imgPad->cols, 1, CV_32FC1));
+            cv::Mat colSum(cv::Mat::zeros(imgPad.cols, 1, CV_32FC1));
+
+            cv::Size size(imgPad.rows, imgPad.cols);
 
             for (int angle = 0; angle != RADON_DEGREE_RANGE; angle ++) {
-                rotationMatrix = cv::getRotationMatrix2D(center, 1.0, 1.0);
-                cv::warpAffine(*imgPad, *imgPad, rotationMatrix, cv::Size(imgPad->rows, imgPad->cols), INTER_CUBIC, BORDER_TRANSPARENT, 0);
+                rotationMatrix = cv::getRotationMatrix2D(center, angle, 1.0);
+                cv::warpAffine(imgPad, imgPadRotated, rotationMatrix, size, INTER_LINEAR | WARP_INVERSE_MAP, BORDER_TRANSPARENT);
 
-                cv::reduce(*imgPad, colSum, 0, CV_REDUCE_SUM, CV_32FC1);
-
+                cv::reduce(imgPadRotated, colSum, 0, CV_REDUCE_SUM, CV_32FC1);
                 sinogram->push_back(colSum);
-                //colSum.col(angle).copyTo(sinogram->col(angle));
-                //sinogram->col(angle) = colSum * 1;
-
             }
 
             delete data;
 
-            _loaderData.images->at(i) = imgPad;
+            _loaderData.images->at(i) = contourImage;
 
             _loaderData.sinograms->at(i) = sinogram;
-*/
+
             //cv::medianBlur(*data8, *contourImage, 5);
             //cv::Canny(*contourImage, *contourImage, CANNY_LOWER, 3 * CANNY_LOWER, 3);
 
@@ -176,17 +173,15 @@ public:
             //stegerEdges(*contourImage, *steger, 5, 0, 10.0);
 
             //cv::adaptiveThreshold(*contourImage, *contourImage, 200, CV_ADAPTIVE_THRESH_GAUSSIAN_C, CV_THRESH_BINARY, 3, 1);
-            /*
-            cv::findContours(*contourImage, contours, hierarchy, CV_RETR_CCOMP, CV_CHAIN_APPROX_TC89_KCOS, cv::Point(0, 0));
+/*
+            cv::findContours(data8, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_TC89_KCOS, cv::Point(0, 0));
 
             for (uint k = 0; k < contours.size(); k ++) {
                 if (contours.at(k).size() > 300) {
                     cv::drawContours(*contourImage, contours, k, cv::Scalar(0x00FF), 2, 8, hierarchy, 0, cv::Point());
                 }
             }
-            */
-
-
+*/
             //oclData->download(*contourImage);
 
             //delete contourImage;
@@ -442,9 +437,9 @@ void DicomReader::incImageNumber() {
 
 void DicomReader::showImageWithNumber(const int & imageNumber) {
     cv::imshow(WINDOW_CONTOUR_IMAGE, *(_ctImages[imageNumber]));
-    cv::imshow(WINDOW_DICOM_IMAGE, *(_images[imageNumber]));
-
-    cv::imshow(WINDOW_RADON_2D, *_sinogram);
+    //cv::imshow(WINDOW_DICOM_IMAGE, *(_images[imageNumber]));
+    cv::imshow(WINDOW_RADON_2D, *(_sinograms[imageNumber]));
+    //cv::imshow(WINDOW_RADON_2D, *_sinogram);
     cv::waitKey(1);
 }
 
